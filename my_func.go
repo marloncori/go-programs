0d16ef6a14cf09b30afcf9bbc7979cc0720e06ed
marloncori <84450525+marloncori@users.noreply.github.com>
2021-11-24 12:53:04 +0100

    Update my_func.go

--- my_func.go
+++ my_func.go
@@ -19,56 +19,63 @@ func addThree(a int, b int, c int) int {
 
 func mult_divide(m int, n int) (int, int) {
    mul := m * n
+   div := 0
     if m > n {
-     div := m / n
-    } else
-     div := n / m
+     div = m / n
+    } else {
+     div = n / m
+    }
   return mul, div
 }
 
 func main() {
   
   choice := os.Args[1]
-  elem1 := 0
-  elem2, elem3 := 0, 0
+  elem1 := os.Args[2]
+  elem2 := os.Args[3]
+  elem3 := os.Args[4]
   
   if choice == "1" {
-    elem1 _ = strconv.Atoi(os.Args[2])
-    elem2, _ = strconv.Atoi(os.Args[3])
+    elem1, err = strconv.Atoi(elem1)
+    elem2, err = strconv.Atoi(elem2)
     
-    result := plus(elem1, elem2)
-    fmt.Printf("The result of adding %d to %d equals:\n", elem1, elem2)
+    if err == nil {
+      result := plus(elem1, elem2)
+      fmt.Printf("The result of adding %d to %d equals:\n", elem1, elem2)
     
-    time.Sleep(1*time.Second)
-    fmt.Println("\n", os.Args[2], " + ", os.Args[3], " = ", result)
-    
-  } else if choice == "2" {
-    elem1 _ = strconv.Atoi(os.Args[2])
-    elem2, _ = strconv.Atoi(os.Args[3])
-    elem3 _ = strconv.Atoi(os.Args[4])
-    
-    res := addThree(elem1, elem2, elem3)
-    fmt.Printf("The result of adding %d and %d to %d equals:\n", elem1, elem2, elem3)
-    
-    time.Sleep(1*time.Second)
-    fmt.Println("\n", os.Args[2], " + ", os.Args[3], " + ", os.Args[4], " = ", res)
-    
-  } else {
-    elem1 _ = strconv.Atoi(os.Args[2])
-    elem2, _ = strconv.Atoi(os.Args[3])
-    
-    result1, result2 := mult_divide(elem1, elem2)
-    fmt.Printf("The result of multiplying, dividing %d and %d equals:\n", elem1, elem2)
-    
-    time.Sleep(1*time.Second)
-    fmt.Println("\n", os.Args[2], " * ", os.Args[3], " = ", result1)
-    
-    if elem1 > elem2 {
       time.Sleep(1*time.Second)
-      fmt.Println("\n", os.Args[2], " / ", os.Args[3], " = ", result2)
-    } else {
-      time.Sleep(1*time.Second)
-      fmt.Println("\n", os.Args[3], " / ", os.Args[2], " = ", result2)
+      fmt.Println("\n", os.Args[2], " + ", os.Args[3], " = ", result)
     }
+  } else if choice == "2" {
+    elem1, err = strconv.Atoi(elem1)
+    elem2, err = strconv.Atoi(elem2)
+    elem3, err = strconv.Atoi(elem3)
+    
+    if err == nil {
+      res := addThree(elem1, elem2, elem3)
+      fmt.Printf("The result of adding %d and %d to %d equals:\n", elem1, elem2, elem3)
+    
+      time.Sleep(1*time.Second)
+      fmt.Println("\n", os.Args[2], " + ", os.Args[3], " + ", os.Args[4], " = ", res)
+    }
+  } else {
+    elem1, err = strconv.Atoi(elem1)
+    elem2, err = strconv.Atoi(elem2)
+    
+    if err == nil {
+      result1, result2 := mult_divide(elem1, elem2)
+      fmt.Printf("The result of multiplying, dividing %d and %d equals:\n", elem1, elem2)
+    
+      time.Sleep(1*time.Second)
+      fmt.Println("\n", os.Args[2], " * ", os.Args[3], " = ", result1)
+    
+      if elem1 > elem2 {
+        time.Sleep(1*time.Second)
+        fmt.Println("\n", os.Args[2], " / ", os.Args[3], " = ", result2)
+       } else {
+        time.Sleep(1*time.Second)
+        fmt.Println("\n", os.Args[3], " / ", os.Args[2], " = ", result2)
+      }
+    } 
   }
 }
